d0f8cc02f37c6a45868bf2a19f2d24190935b5d8
Alexander Salois <asalois.scholar@gmail.com>
2021-08-23 15:46:05 -0600

    added more varibles for slurm template

--- run.py
+++ run.py
@@ -2,11 +2,15 @@ from mako.template import Template
 my_tmpl = Template(filename='slurm_tmpl.txt')
 test_cv = ["cv","test"]
 script_lines = "#!/bin/bash \n"
+py_file_path = "/mnt/lustrefs/scratch/v16b915/pof_nns/"
+memory = 4000
+time_limit = 3 * 60
 
-for i in range(1,6):
+for i in range(1,7):
     for test_or_cv in test_cv: 
-        py_file = "deep_nn_" + test_or_cv +"_hpc.py"
-        lines = my_tmpl.render(test_cv=test_or_cv,sample=str(i),py_file=py_file) 
+        py_file = py_file_path + "deep_nnEq_" + test_or_cv +"_hpc.py"
+        lines = my_tmpl.render(test_cv=test_or_cv,sample=str(i),py_file=py_file,\
+        mem=str(memory),time=str(time_limit),queue="defq") 
         lines += "\n"
         file_name = test_or_cv + str(i).zfill(2) + ".slurm"
         script_lines += "sbatch " + file_name + "\n"  
