4fbaa4a83417acac12b9972cfa1bf7f596e7b8f3
Alexander Salois <asalois.scholar@gmail.com>
2021-08-28 11:56:20 -0600

    adjusted for 10 samples now

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ my_tmpl = Template(filename='slurm_tmpl.txt')
 test_cv = ["cv","test"]
 script_lines = "#!/bin/bash \n"
 py_file_path = "/mnt/lustrefs/scratch/v16b915/pof_nns/"
-memory = 4000
-time_limit = 3 * 60
+memory = 8000
+time_limit = 20 * 60
 
-for i in range(1,7):
+for i in range(1,11):
     for test_or_cv in test_cv: 
         py_file = py_file_path + "deep_nnEq_" + test_or_cv +"_hpc.py"
         lines = my_tmpl.render(test_cv=test_or_cv,sample=str(i),py_file=py_file,\
