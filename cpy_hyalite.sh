4b17d070a9e5d00f9fe81357e82791a91cc344a4
Alexander Salois <asalois.scholar@gmail.com>
2021-08-30 15:35:05 -0600

    copy slurms to hyalitye hpc

---
+++ cpy_hyalite.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp *.slurm v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/pof_nns
+scp launch_nns.sh v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/pof_nns
+
