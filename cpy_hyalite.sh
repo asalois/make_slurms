f641c555299033e10aae01b492410233cce37b62
Alexander Salois <asalois.scholar@gmail.com>
2021-08-31 17:10:21 -0600

    start of new thing

--- cpy_hyalite.sh
+++ cpy_hyalite.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp *.slurm launch_nns.sh v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/pof_nns
+scp *.slurm launch_nns.sh v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/pof_nns/fiber100/e10_12tanh_12relu
 
---
+++ process_data.py
@@ -0,0 +1,22 @@
+from mako.template import Template
+my_tmpl = Template(filename='slurm_tmpl.txt')
+script_lines = "#!/bin/bash \n"
+py_file_path = "/mnt/lustrefs/scratch/v16b915/pof_nns/fiber100/e10_12tanh_12relu/"
+memory = 4 * 1024
+time_limit = 15 * 60
+
+for i in range(1,11):
+    py_file_0 = py_file_path + "deep_nnEq_cv_hpc.py"
+    py_file_1 = py_file_path + "deep_nnEq_test_hpc.py"
+    lines = my_tmpl.render(sample=str(i),py_file_0=py_file_0,\
+    py_file_1=py_file_1, mem=str(memory),time=str(time_limit),queue="defq") 
+    lines += "\n"
+    file_name = str(i).zfill(2) + "_samples.slurm"
+    script_lines += "sbatch " + file_name + "\n"  
+    file = open(file_name,"w+")
+    file.writelines(lines)
+
+
+file = open("launch_nns.sh","w+")
+file.writelines(script_lines)        
+
