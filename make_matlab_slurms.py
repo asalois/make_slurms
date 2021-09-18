3d271035b84625faea65888e39932f631c4d74bf
Alexander Salois <asalois.scholar@gmail.com>
2021-09-17 18:26:31 -0600

    adjusted time

--- make_matlab_slurms.py
+++ make_matlab_slurms.py
@@ -2,25 +2,21 @@ from mako.template import Template
 my_tmpl = Template(filename='slurm_matlab_tmpl.txt')
 my_scp_tmpl = Template(filename='cpy_hyalite_tmpl.txt')
 memory = 6 * 1024
-time = 120
-partition = "defq"
-mfile_name = "dfe_scan"
+time = 15
+partition = "unsafe"
+mfile_name = "scan_dfe"
 job_name = "ber_dfe"
 script_lines = "#!/bin/bash \n"
 x_dir = "/mnt/lustrefs/scratch/v16b915/dfe_scan/"
 last = 3542
 
 for i in range(1,5): 
-        start = (i-1)*1000 + 1
-        end = i*1000
-        if end > last:
-            end = last
+        start = 1
+        end = 1000
+        if i == 4:
+            end = last - 3000
 
-        if i == 1:
-            pl = 0
-        else:
-            pl = 1000
-
+        pl = 1000*(i-1)
         lines = my_tmpl.render(mem=memory,time=time,queue=partition,\
             start_ar=start,end_ar=end,m_file=mfile_name,j_name=job_name,plus=pl) 
         lines += "\n"
