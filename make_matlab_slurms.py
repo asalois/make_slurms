f60ce8a3590ae2b70b497e74516257eac74746c8
Alexander Salois <asalois.scholar@gmail.com>
2021-09-28 22:14:51 -0600

    more progress

--- make_matlab_slurms.py
+++ make_matlab_slurms.py
@@ -1,16 +1,16 @@
 from mako.template import Template
 my_tmpl = Template(filename='slurm_matlab_tmpl.txt')
 my_scp_tmpl = Template(filename='cpy_hyalite_tmpl.txt')
-memory = 6 * 1024
-time = 15
-partition = "unsafe"
+memory = 4 * 1024
+time = 30
+partition = "defq"
 mfile_name = "scan_dfe"
 job_name = "ber_dfe"
 script_lines = "#!/bin/bash \n"
-x_dir = "/mnt/lustrefs/scratch/v16b915/dfe_scan/"
+x_dir = "/mnt/lustrefs/scratch/v16b915/dfe_scan_new/"
 last = 3542
 
-for i in range(1,5): 
+for i in range(16): 
         start = 1
         end = 1000
         if i == 4:
@@ -18,9 +18,9 @@ for i in range(1,5):
 
         pl = 1000*(i-1)
         lines = my_tmpl.render(mem=memory,time=time,queue=partition,\
-            start_ar=start,end_ar=end,m_file=mfile_name,j_name=job_name,plus=pl) 
+            num=i,plus=pl) 
         lines += "\n"
-        file_name = "dfe_ber_" + str(i) + ".slurm"
+        file_name = "dfe_ber_" + str(i).zfill(2) + ".slurm"
         script_lines += "sbatch " + file_name + "\n"  
         file = open(file_name,"w+")
         file.writelines(lines)
--- to_hyalite.sh
+++ to_hyalite.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp *.slurm launch*.sh v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/dfe_scan/
+scp *.slurm launch*.sh v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/dfe_scan_new/
 
