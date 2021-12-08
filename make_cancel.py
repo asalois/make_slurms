45cc58e2ff0aa096635a59a7815fd46fcc7c9f48
Alexander Salois <asalois.scholar@gmail.com>
2021-12-08 09:12:18 -0700

    doing some more work

--- make_cancel.py
+++ make_cancel.py
@@ -2,11 +2,11 @@ from mako.template import Template
 my_tmpl = Template(filename='slurm_matlab_tmpl.txt')
 my_scp_tmpl = Template(filename='cpy_hyalite_tmpl.txt')
 script_lines = "#!/bin/bash \n"
-x_dir = "/mnt/lustrefs/scratch/v16b915/dfe_scan_new/"
+x_dir = "/mnt/lustrefs/scratch/v16b915/pthreads/"
 
-end = 64
-for i in range(end + 1): 
-        job_name = "berDFE" + str(i).zfill(2) 
+end = 40
+for i in range(41,100): 
+        job_name = "PAM" + str(i).zfill(2) 
         script_lines += "scancel -n " + job_name + "\n"  
 
 
